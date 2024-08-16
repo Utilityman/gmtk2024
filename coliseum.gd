4a1f0decea3a5715beedc9c38934da5d41a85285
Josh Mackin <mackin711@gmail.com>
2024-08-16 17:32:36 -0400

    player stuff in the coliseum

--- coliseum.gd
+++ coliseum.gd
@@ -1,9 +1,12 @@
 extends Node3D
 
 
+@onready var player: Entity = $Player
+@onready var ui: PlayerUserInterface = $Player/PlayerUI
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player.ui = ui
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
