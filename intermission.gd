06cb56653f145a5c324d5f88319a28eb6dc84b69
Josh Mackin <cmackin712@gmail.com>
2024-08-18 13:48:30 -0400

    intermission work

--- intermission.gd
+++ intermission.gd
@@ -3,21 +3,34 @@ extends Node3D
 
 @onready var timer: Timer = $Timer
 @onready var timer_label: Label = $Control/Timer
-@onready var arms1_button: TextureButton = $Control/Control/PanelContainer/GridContainer/Arm1
-@onready var arms2_button: TextureButton = $Control/Control/PanelContainer/GridContainer/Arm1
-@onready var arms3_button: TextureButton = $Control/Control/PanelContainer/GridContainer/Arm1
-@onready var head1_button: TextureButton = $Control/Control/PanelContainer/GridContainer/Arm1
-@onready var head2_button: TextureButton = $Control/Control/PanelContainer/GridContainer/Arm1
-@onready var head3_button: TextureButton = $Control/Control/PanelContainer/GridContainer/Arm1
+@onready var arms1_button: TextureButton = $Control/PanelContainer/GridContainer/Arm1
+@onready var arms2_button: TextureButton = $Control/PanelContainer/GridContainer/Arm2
+@onready var arms3_button: TextureButton = $Control/PanelContainer/GridContainer/Arm3
+@onready var head1_button: TextureButton = $Control/PanelContainer/GridContainer/Head1
+@onready var head2_button: TextureButton = $Control/PanelContainer/GridContainer/Head2
+@onready var head3_button: TextureButton = $Control/PanelContainer/GridContainer/Head3
+@onready var player_body: Skeleton3D = $Player/RobotModel/RootNode/CharacterArmature/Skeleton3D
 
 var player_data: PlayerData = Players.player
+var target_scale: Vector3 = Vector3(2, 2, 2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer.start(shop_time)
 	print(player_data)
-	# if player_data.money < 60:
-	# 	arms1_button.disabled = true
+	print(player_body)
+	if player_data.money < 60 or player_data.arms == 1:
+		arms1_button.disabled = true
+	if player_data.money < 60:
+		head1_button.disabled = true
+	if player_data.money < 70:
+		arms2_button.disabled = true
+	if player_data.money < 70:
+		head2_button.disabled = true
+	if player_data.money < 80:
+		arms3_button.disabled = true
+	if player_data.money < 80:
+		head3_button.disabled = true
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -38,27 +51,103 @@ func _on_continue_button_pressed() -> void:
 
 #all money values are hard coded rn and super made up
 func _on_arm_v2_pressed() -> void:
-	#player_data.money = player_data.money - 60
+	print("purchased arms vo. 2")
+	player_data.money = player_data.money - 60
 	player_data.arms = 1
+	var bone_index_r: int = player_body.find_bone("Shoulder.R")
+	var bone_index_l: int = player_body.find_bone("Shoulder.L")
+
+	var pose_r: Transform3D = player_body.get_bone_pose(bone_index_r)
+	var pose_l: Transform3D = player_body.get_bone_pose(bone_index_l)
+
+	var scale: int = 3
+	pose_r = pose_r.scaled(Vector3(scale, scale, scale))
+	pose_l = pose_l.scaled(Vector3(scale, scale, scale))
+
+	player_body.set_bone_pose(bone_index_r, pose_r)
+	player_body.set_bone_pose(bone_index_l, pose_l)
+
+	arms1_button.disabled = true
+	
 
 func _on_head_1_pressed() -> void:
-	#player_data.money = player_data.money - 60
+	player_data.money = player_data.money - 60
 	player_data.head = 1
+	var head_scale: int = 3
+	var bone_index: int = player_body.find_bone("Head")
+	var pose: Transform3D = player_body.get_bone_pose(bone_index)
+	pose = pose.scaled(Vector3(head_scale, head_scale, head_scale))
+	player_body.set_bone_pose(bone_index, pose)
+	var arms_scale: float = 0.3333
+	var bone_index_r: int = player_body.find_bone("Shoulder.R")
+	var bone_index_l: int = player_body.find_bone("Shoulder.L")
+
+	var pose_r: Transform3D = player_body.get_bone_pose(bone_index_r)
+	var pose_l: Transform3D = player_body.get_bone_pose(bone_index_l)
+
+	pose_r = pose_r.scaled(Vector3(arms_scale, arms_scale, arms_scale))
+	pose_l = pose_l.scaled(Vector3(arms_scale, arms_scale, arms_scale))
+
+	player_body.set_bone_pose(bone_index_r, pose_r)
+	player_body.set_bone_pose(bone_index_l, pose_l)
+	
+	
+	head1_button.disabled = true
+	
+	
+	
+		
+
 
 func _on_arm_2_pressed() -> void:
-	#player_data.money = player_data.money - 80
+	player_data.money = player_data.money - 80
 	player_data.arms = 2
+	print("purchased arms vo. 3")
+	var bone_index_r: int = player_body.find_bone("Shoulder.R")
+	var bone_index_l: int = player_body.find_bone("Shoulder.L")
+
+	var pose_r: Transform3D = player_body.get_bone_pose(bone_index_r)
+	var pose_l: Transform3D = player_body.get_bone_pose(bone_index_l)
+
+	var scale: float = 1.67
+	pose_r = pose_r.scaled(Vector3(scale, scale, scale))
+	pose_l = pose_l.scaled(Vector3(scale, scale, scale))
+
+	player_body.set_bone_pose(bone_index_r, pose_r)
+	player_body.set_bone_pose(bone_index_l, pose_l)
+
+	arms2_button.disabled = true
+	
 
 func _on_head_2_pressed() -> void:
-	#player_data.money = player_data.money - 80
-	player_data.head = 2
+	player_data.money = player_data.money - 60
+	player_data.head = 1
+	var head_scale: int = 1.67
+	var bone_index: int = player_body.find_bone("Head")
+	var pose: Transform3D = player_body.get_bone_pose(bone_index)
+	pose = pose.scaled(Vector3(head_scale, head_scale, head_scale))
+	player_body.set_bone_pose(bone_index, pose)
+	var arms_scale: float = 0.2
+	var bone_index_r: int = player_body.find_bone("Shoulder.R")
+	var bone_index_l: int = player_body.find_bone("Shoulder.L")
+
+	var pose_r: Transform3D = player_body.get_bone_pose(bone_index_r)
+	var pose_l: Transform3D = player_body.get_bone_pose(bone_index_l)
+
+	pose_r = pose_r.scaled(Vector3(arms_scale, arms_scale, arms_scale))
+	pose_l = pose_l.scaled(Vector3(arms_scale, arms_scale, arms_scale))
+
+	player_body.set_bone_pose(bone_index_r, pose_r)
+	player_body.set_bone_pose(bone_index_l, pose_l)
+	
+	
+	head1_button.disabled = true
+	
 
 func _on_arm_3_pressed() -> void:
-	#player_data.money = player_data.money - 100
+	player_data.money = player_data.money - 100
 	player_data.arms = 3
 
 func _on_head_3_pressed() -> void:
-	#player_data.money = player_data.money - 100
+	player_data.money = player_data.money - 100
 	player_data.head = 3
-
-
--- player_data.gd
+++ player_data.gd
@@ -3,7 +3,17 @@ class_name PlayerData extends Resource
 @export var data: RpgData
 @export var punch_ability: Ability
 @export var shoot_ability: Ability
-@export var money: int = 0
+@export var money: int = 100
 @export var arms: int = 0 #0-4 levels
 @export var head: int = 0 #0-4 levels
 # is this where we'll collect the nuts/bolts resources?
+
+
+func get_arms_scale () -> float:
+    # TMP TODO
+    if arms == 1 and head == 0: return 3.0
+    if arms == 1 and head == 1: return 1 / 3.0
+    return 1.0
+
+func get_player_capsule_height () -> float:
+    return 2.0
