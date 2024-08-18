81daf37a32f7fece57d6f93278ea446e735066d4
Josh Mackin <cmackin712@gmail.com>
2024-08-18 16:11:29 -0400

    shop scene done mostly

--- intermission.gd
+++ intermission.gd
@@ -3,13 +3,16 @@ extends Node3D
 
 @onready var timer: Timer = $Timer
 @onready var timer_label: Label = $Control/Timer
-@onready var arms1_button: TextureButton = $Control/PanelContainer/GridContainer/Arm1
-@onready var arms2_button: TextureButton = $Control/PanelContainer/GridContainer/Arm2
-@onready var arms3_button: TextureButton = $Control/PanelContainer/GridContainer/Arm3
-@onready var head1_button: TextureButton = $Control/PanelContainer/GridContainer/Head1
-@onready var head2_button: TextureButton = $Control/PanelContainer/GridContainer/Head2
-@onready var head3_button: TextureButton = $Control/PanelContainer/GridContainer/Head3
+@onready var arms1_button: Button = $Control/PanelContainer/GridContainer/Arm1
+@onready var arms2_button: Button = $Control/PanelContainer/GridContainer/Arm2
+@onready var arms3_button: Button = $Control/PanelContainer/GridContainer/Arm3
+@onready var head1_button: Button = $Control/PanelContainer/GridContainer/Head1
+@onready var head2_button: Button = $Control/PanelContainer/GridContainer/Head2
+@onready var head3_button: Button = $Control/PanelContainer/GridContainer/Head3
 @onready var player_body: Skeleton3D = $Player/RobotModel/RootNode/CharacterArmature/Skeleton3D
+@onready var player_collision: CollisionShape3D = $Player/Collision
+@onready var left_hand_collision : CollisionShape3D = $Player/RobotModel/RootNode/CharacterArmature/Skeleton3D/BoneAttachment3D/Area3D/CollisionShape3D
+@onready var right_hand_collision : CollisionShape3D = $Player/RobotModel/RootNode/CharacterArmature/Skeleton3D/BoneAttachment3D2/Area3D/CollisionShape3D
 
 var player_data: PlayerData = Players.player
 var target_scale: Vector3 = Vector3(2, 2, 2)
@@ -17,26 +20,28 @@ var target_scale: Vector3 = Vector3(2, 2, 2)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer.start(shop_time)
+	print(left_hand_collision)
+	print(right_hand_collision)
 	print(player_data)
 	print(player_body)
-	if player_data.money < 60 or player_data.arms == 1:
-		arms1_button.disabled = true
-	if player_data.money < 60:
-		head1_button.disabled = true
-	if player_data.money < 70:
-		arms2_button.disabled = true
-	if player_data.money < 70:
-		head2_button.disabled = true
-	if player_data.money < 80:
-		arms3_button.disabled = true
-	if player_data.money < 80:
-		head3_button.disabled = true
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	timer_label.text = "%d:%02d" % [floor($Timer.time_left / 60), int($Timer.time_left) % 60]
 	if timer.time_left < 30.0:
 		timer_label.text = "%10.1f" % timer.time_left
+	if player_data.money < 60 or player_data.arms >= 1:
+		arms1_button.disabled = true
+	if player_data.money < 60 or player_data.head >=1 :
+		head1_button.disabled = true
+	if player_data.money < 70 or player_data.arms != 1:
+		arms2_button.disabled = true
+	if player_data.money < 70 or player_data.head != 1:
+		head2_button.disabled = true
+	if player_data.money < 80 or player_data.arms != 2:
+		arms3_button.disabled = true
+	if player_data.money < 80 or  player_data.head != 2:
+		head3_button.disabled = true
 
 func _on_timer_timeout() -> void:
 	print("OUT OF TIME IN SHOP!")
@@ -67,7 +72,18 @@ func _on_arm_v2_pressed() -> void:
 	player_body.set_bone_pose(bone_index_r, pose_r)
 	player_body.set_bone_pose(bone_index_l, pose_l)
 
+	#Increase collision shape here
+	var collison_scale: float = 1.25 
+	player_collision.scale = Vector3(collison_scale, collison_scale, collison_scale)
+
+	#Decrease hand collision shapes
+
+	var hand_shape: SphereShape3D = left_hand_collision.shape
+	hand_shape.radius = 0.0015
+	left_hand_collision.shape = hand_shape
+	right_hand_collision.shape = hand_shape
 	arms1_button.disabled = true
+	arms2_button.disabled = false
 	
 
 func _on_head_1_pressed() -> void:
@@ -91,14 +107,11 @@ func _on_head_1_pressed() -> void:
 	player_body.set_bone_pose(bone_index_r, pose_r)
 	player_body.set_bone_pose(bone_index_l, pose_l)
 	
-	
 	head1_button.disabled = true
+	head2_button.disabled = false
 	
 	
 	
-		
-
-
 func _on_arm_2_pressed() -> void:
 	player_data.money = player_data.money - 80
 	player_data.arms = 2
@@ -116,18 +129,22 @@ func _on_arm_2_pressed() -> void:
 	player_body.set_bone_pose(bone_index_r, pose_r)
 	player_body.set_bone_pose(bone_index_l, pose_l)
 
+	var collison_scale: float = 1.5
+	player_collision.scale = Vector3(collison_scale, collison_scale, collison_scale)
+
 	arms2_button.disabled = true
-	
+	arms3_button.disabled = false
+		
 
 func _on_head_2_pressed() -> void:
 	player_data.money = player_data.money - 60
-	player_data.head = 1
-	var head_scale: int = 1.67
+	player_data.head = 2
+	var head_scale: float = 1.67
 	var bone_index: int = player_body.find_bone("Head")
 	var pose: Transform3D = player_body.get_bone_pose(bone_index)
 	pose = pose.scaled(Vector3(head_scale, head_scale, head_scale))
 	player_body.set_bone_pose(bone_index, pose)
-	var arms_scale: float = 0.2
+	var arms_scale: float = 0.599
 	var bone_index_r: int = player_body.find_bone("Shoulder.R")
 	var bone_index_l: int = player_body.find_bone("Shoulder.L")
 
@@ -140,14 +157,52 @@ func _on_head_2_pressed() -> void:
 	player_body.set_bone_pose(bone_index_r, pose_r)
 	player_body.set_bone_pose(bone_index_l, pose_l)
 	
-	
-	head1_button.disabled = true
+	head2_button.disabled = true
+	head3_button.disabled = false
 	
 
 func _on_arm_3_pressed() -> void:
 	player_data.money = player_data.money - 100
 	player_data.arms = 3
+	print("purchased arms vo. 4")
+	var bone_index_r: int = player_body.find_bone("Shoulder.R")
+	var bone_index_l: int = player_body.find_bone("Shoulder.L")
+
+	var pose_r: Transform3D = player_body.get_bone_pose(bone_index_r)
+	var pose_l: Transform3D = player_body.get_bone_pose(bone_index_l)
+
+	var scale: float = 1.4
+	pose_r = pose_r.scaled(Vector3(scale, scale, scale))
+	pose_l = pose_l.scaled(Vector3(scale, scale, scale))
+
+	player_body.set_bone_pose(bone_index_r, pose_r)
+	player_body.set_bone_pose(bone_index_l, pose_l)
+
+	var collison_scale: float = 1.75
+	player_collision.scale = Vector3(collison_scale, collison_scale, collison_scale)
+
+	arms3_button.disabled = true
 
 func _on_head_3_pressed() -> void:
 	player_data.money = player_data.money - 100
 	player_data.head = 3
+	var head_scale: float = 1.44
+	var bone_index: int = player_body.find_bone("Head")
+	var pose: Transform3D = player_body.get_bone_pose(bone_index)
+	pose = pose.scaled(Vector3(head_scale, head_scale, head_scale))
+	player_body.set_bone_pose(bone_index, pose)
+	var arms_scale: float = 0.694
+	var bone_index_r: int = player_body.find_bone("Shoulder.R")
+	var bone_index_l: int = player_body.find_bone("Shoulder.L")
+
+	var pose_r: Transform3D = player_body.get_bone_pose(bone_index_r)
+	var pose_l: Transform3D = player_body.get_bone_pose(bone_index_l)
+
+	pose_r = pose_r.scaled(Vector3(arms_scale, arms_scale, arms_scale))
+	pose_l = pose_l.scaled(Vector3(arms_scale, arms_scale, arms_scale))
+
+	player_body.set_bone_pose(bone_index_r, pose_r)
+	player_body.set_bone_pose(bone_index_l, pose_l)
+	
+	head3_button.disabled = true
+	
--- player_data.gd
+++ player_data.gd
@@ -3,17 +3,22 @@ class_name PlayerData extends Resource
 @export var data: RpgData
 @export var punch_ability: Ability
 @export var shoot_ability: Ability
-@export var money: int = 100
+@export var money: int = 1000
 @export var arms: int = 0 #0-4 levels
 @export var head: int = 0 #0-4 levels
 # is this where we'll collect the nuts/bolts resources?
 
 
 func get_arms_scale () -> float:
-    # TMP TODO
-    if arms == 1 and head == 0: return 3.0
-    if arms == 1 and head == 1: return 1 / 3.0
-    return 1.0
+    if head == 0:
+        return 1
+    if head == 1:
+        return 0.3333
+    if head == 2:
+        return 0.5999
+    if head == 3:
+        return 0.6944
+    return 0.0
 
 func get_player_capsule_height () -> float:
     return 2.0
